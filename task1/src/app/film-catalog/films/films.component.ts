49fd35db0ab798a2e7537dac245349dcef7b848c
Olga Gnatenko <olgagnat13@gmail.com>
2018-06-18 12:30:34 +0300

    Task1 Complete
    Completed task 1 (both parts)

--- task1/src/app/film-catalog/films/films.component.ts
+++ task1/src/app/film-catalog/films/films.component.ts
@@ -2,21 +2,19 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FilmService } from '../film.service';
 
 @Component({
-  selector: '.films',
+  selector: 'films',
   templateUrl: './films.component.html',
   styleUrls: ['./films.component.css']
 })
 export class FilmsComponent implements OnInit {
 
-  description: string = 'Middle card description';
-  
+  films: object[];
   
   constructor(filmsService: FilmService) { 
-    
+    this.films  = filmsService.films ;
   }
   
   ngOnInit() { 
-    
   }
   
 }
--- task1/src/app/film-catalog/main/main.component.ts
+++ task1/src/app/film-catalog/main/main.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
-  //styles: [`h2 {color: red;}`, ``]
 })
 export class MainComponent implements OnInit {
   pageName: string = "Film Catalog Dashboard";
